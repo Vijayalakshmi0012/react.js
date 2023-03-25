7c820cb2fdd382c3a4173267668991b90b006acc
Vijayalakshmi0012 <120468347+Vijayalakshmi0012@users.noreply.github.com>
2023-03-25 20:15:53 +0530

    Add files via upload

---
+++ Forms3.jsx
@@ -0,0 +1,31 @@
+
+import { useState } from 'react';
+const Forms3=()=>{
+    let[user,setUser]=useState("")
+    let[pass,setPass]=useState("")
+    let handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log({user,pass});
+    }
+    return(
+
+        <div>
+            <form action=""onSubmit={handleSubmit}>
+                <label htmlFor='username'>Username</label><br/>
+                <input type="text"id="username"onChange={(e)=>{
+                    setUser(e.target.value)
+                }
+                }/>
+                <br/>
+                <label htmlFor='password'>Password</label><br/>
+                <input type="password"id="password"onChange={(e)=>{
+                    setPass(e.target.value)
+                }
+                }/>
+                <br/>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+export default Forms3
